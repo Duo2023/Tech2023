8c84017e11cab6fea996a37681d9f7aaa6693343
Bradley Grover <ac111897@avcol.school.nz>
2023-02-12 12:20:54 +1300

    Add benchmarks executable for later use

---
+++ src/Tech2023.Benchmarking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tech2023.Benchmarking;
+
+/* src/Tech2023.Benchmarking/Program.cs */
+/* benchmarks will be automatically discovered and can be chosen to run when executed */
+
+public sealed class Program
+{
+    public static void Main(string[] args) 
+        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
