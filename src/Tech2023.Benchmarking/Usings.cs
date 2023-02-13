c71dee461a6e8c88c07414a2155eb315eed8fbb8
Bradley Grover <76420458+ac111897@users.noreply.github.com>
2023-02-13 13:51:21 +1300

    Add benchmarks executable into main (#2)
    * Add benchmarks executable for later use
    
    * Add workflow support for /dev

---
+++ src/Tech2023.Benchmarking/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using BenchmarkDotNet.Running;
+global using BenchmarkDotNet.Attributes;
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static",
+    Justification = "Benchmark assembly has mutliple methods that are static in implementation but cannot be static due to BenchmarkDotNet")]
