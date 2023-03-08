9cea7cd570ec801dc6a5a3d169463d75c8e07404
Bradley Grover <ac111897@avcol.school.nz>
2023-03-09 11:03:53 +1300

    change to MVC & Web API and add MicrosoftPackageVersion

---
+++ Tech2023.Web.API/Startup.cs
@@ -0,0 +1,9 @@
+﻿namespace Tech2023.Web.API;
+
+public sealed class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+    }
+}
--- src/Tech2023.Web/Server/Usings.cs
+++
@@ -1,1 +0,0 @@
-﻿global using Tech2023.DAL;
