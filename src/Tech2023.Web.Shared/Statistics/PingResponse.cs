700df6e3e690c26f946b45ab9cc4de0301003ce7
Bradley Grover <ac111897@avcol.school.nz>
2023-04-06 11:09:56 +1200

    convert PingResponse to class for JSON initialization

--- src/Tech2023.Web.Shared/Statistics/PingResponse.cs
+++ src/Tech2023.Web.Shared/Statistics/PingResponse.cs
@@ -1,65 +1,28 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Net;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using Tech2023.Core.Json.Converters;
+
+#nullable disable
 
 namespace Tech2023.Web.Shared.Statistics;
 
-public readonly struct PingResponse : IEquatable<PingResponse>
+public class PingResponse
 {
-    internal readonly TimeSpan _runtime;
-    internal readonly string _ipAddress;
-
-    public PingResponse(TimeSpan runtime, string ipAddress)
-    {
-        _runtime = runtime;
-        _ipAddress = ipAddress;
-    }
-
-    [JsonPropertyName("runtime")]
-    [JsonConverter(typeof(TimeSpanConverter))]
-    public TimeSpan Runtime
-    {
-        get => _runtime;
-    }
-
-    [JsonPropertyName("ip")]
-    public string Ip
-    {
-        get => _ipAddress;
-    }
-
-    public bool Equals(PingResponse other)
-    {
-        return _runtime == other._runtime;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static PingResponse GetCurrentData(IPAddress address)
-    {
-        return new(TimeSpan.FromTicks(Environment.TickCount64), address.ToString());
-    }
-
-    public override bool Equals([AllowNull] object obj)
-    {
-        return obj is PingResponse response && Equals(response);
-    }
-
-    public static bool operator ==(PingResponse left, PingResponse right)
-    {
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(PingResponse left, PingResponse right)
-    {
-        return !(left == right);
-    }
-
     /// <summary>
-    /// Returns the hashcode of the ping response
+    /// Initializes a new instance of the <see cref="PingResponse"/> class
     /// </summary>
-    public override int GetHashCode()
+    /// <param name="s"></param>
+    /// <param name="ipAddress"></param>
+    public PingResponse(TimeSpan s, string ipAddress)
     {
-        return HashCode.Combine(_runtime, _ipAddress);
+        Runtime = s;
+        Ip = ipAddress;
     }
+
+    [JsonConverter(typeof(TimeSpanConverter))]
+    [JsonPropertyName("runtime")]
+    public TimeSpan Runtime { get; set; }
+
+  
+    [JsonPropertyName("ip")]
+    public string Ip { get; set; }
 }
