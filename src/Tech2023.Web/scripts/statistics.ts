54326865b3574a2f9a2d6cdbba7a6140314dc5f7
Bradley Grover <ac111897@avcol.school.nz>
2023-04-09 23:49:08 +1200

    Configure CORS and add test api

---
+++ src/Tech2023.Web/scripts/statistics.ts
@@ -0,0 +1,30 @@
+﻿export namespace statistics {
+    // Keep the same layout as Web.Shared/Statistics/PingResponse.cs
+    export interface PingResponse {
+        runtime: number; // The json is serialized as a long so keep it as a long here
+        ip: string;
+    }
+
+    export async function isApiUp(): Promise<boolean> {
+        try {
+            const response = await fetch('https://localhost:7098/api/statistics/ping');
+
+            if (!response.ok) {
+                console.error('error getting data from api');
+                return false;
+            }
+
+            const json = await response.text();
+
+            const pingResponse: PingResponse = JSON.parse(json);
+
+            console.log(pingResponse.runtime); // Output the runtime value
+            console.log(pingResponse.ip); // Output the IP value
+
+            return true;
+        } catch (error) {
+            console.error('Error fetching the PingResponse:', error);
+            return false;
+        }
+    }
+}
