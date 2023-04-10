ae8aaaa2839260d3d36548241743099143838428
Bradley Grover <ac111897@avcol.school.nz>
2023-04-10 16:56:36 +1200

    add docs

--- src/Tech2023.Web/scripts/statistics.ts
+++ src/Tech2023.Web/scripts/statistics.ts
@@ -1,10 +1,20 @@
-﻿export namespace statistics {
-    // Keep the same layout as Web.Shared/Statistics/PingResponse.cs
+﻿/**
+ * Contains functions and data types for working with the statistics of the web API
+ */
+export namespace statistics {
+    /**
+     * The same as Web.Shared/Statistics/PingResponse.cs but in TypeScript
+     */
     export interface PingResponse {
         runtime: number; // The json is serialized as a long so keep it as a long here
         ip: string;
     }
 
+     /**
+     * Checks if the API is up and running by sending a ping request to the server.
+     * 
+     * @returns {Promise<boolean>} A Promise that resolves to true if the API is up and running, or false otherwise.
+     */
     export async function isApiUp(): Promise<boolean> {
         try {
             const response = await fetch('https://localhost:7098/api/statistics/ping');
