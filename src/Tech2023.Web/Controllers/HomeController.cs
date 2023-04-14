83852d9bcf7b7fb3b91e7b34e1983d00c8686090
Zennon-Zen <zennon613@gmail.com>
2023-04-14 12:17:48 +1200

    Get Privacy Policy from api

--- src/Tech2023.Web/Controllers/HomeController.cs
+++ src/Tech2023.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Tech2023.DAL.Models;
 using Tech2023.Web.Models;
 
 namespace Tech2023.Web.Controllers;
@@ -7,10 +10,13 @@ namespace Tech2023.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _factory;
+    private PrivacyPolicy? _privacyPolicy { get; set; }
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory factory)
     {
         _logger = logger;
+        _factory = factory;
     }
 
     [Route(Routes.Home)]
@@ -20,10 +26,33 @@ public class HomeController : Controller
     }
 
 
+    private async void GetPrivacyPolicy()
+    {
+        var client = _factory.CreateClient(Clients.API);
+        var response = await client.GetAsync("/api/privacy");
+
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogError("Unsuccessful Privacy Policy API response!\n" +
+                "Api Response: {res}", response);
+            return;
+        }
+
+        PrivacyPolicy? policy = await response.Content.ReadFromJsonAsync<PrivacyPolicy>();
+        if (policy is null)
+        {
+            _logger.LogError("Error parsing Privacy Policy API response!");
+            return;
+        }
+        _privacyPolicy = policy;
+    }
+
     [Route(Routes.Privacy)]
     public IActionResult Privacy()
     {
-        return View();
+        GetPrivacyPolicy();
+        return View(_privacyPolicy);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
