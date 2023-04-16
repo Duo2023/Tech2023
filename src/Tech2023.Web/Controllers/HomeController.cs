641c870a51d0b151c9e087ecb0cc63ca0b873f5e
Bradley Grover <76420458+ac111897@users.noreply.github.com>
2023-04-16 14:26:28 +1200

    Finish Privacy Work for now (#12)
    * Get Privacy Policy from api
    
    * Fix noob mistakes made by yours truly
    
    * Move fonts to a separate file and add title styles
    
    * Style Privacy Policy Page
    
    * optimize with source generation
    
    * Improve readability
    
    * Combine all source generators into one what was i doing lol
    
    * Finish up privacy work
    
    ---------
    
    Co-authored-by: Zennon-Zen <zennon613@gmail.com>

--- src/Tech2023.Web/Controllers/HomeController.cs
+++ src/Tech2023.Web/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Tech2023.DAL.Models;
 using Tech2023.Web.Models;
+using Tech2023.Web.Shared;
 
 namespace Tech2023.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _factory;
+    private static readonly Lazy<PrivacyPolicy> _errorPrivacyPolicy = new(() => new PrivacyPolicy { Content = "Couldn't get Privacy Policy" });
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory factory)
     {
         _logger = logger;
+        _factory = factory;
     }
 
     [Route(Routes.Home)]
@@ -19,11 +24,46 @@ public class HomeController : Controller
         return View();
     }
 
-
     [Route(Routes.Privacy)]
-    public IActionResult Privacy()
+    public async Task<IActionResult> PrivacyAsync()
     {
-        return View();
+        var client = _factory.CreateClient(Clients.API);
+
+        client.Timeout = TimeSpan.FromMilliseconds(500);
+
+        try
+        {
+            var response = await client.GetAsync(ApiRoutes.Privacy.Base);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return Error($"Status Code: {response.StatusCode} sent back by {ApiRoutes.Privacy.Base}");
+            }
+
+            PrivacyPolicy? policy = await response.Content.ReadFromJsonAsync(WebSerializationContext.Default.PrivacyPolicy);
+
+            if (policy is null)
+            {
+                return Error($"API contract broken: Privacy API");
+            }
+
+            return View(policy);
+        }
+        catch (HttpRequestException exception)
+        {
+            return Error(exception.Message);
+        }
+        catch (TaskCanceledException)
+        {
+            return Error("Task was cancelled for retrieving privacy policy because it took too long");
+        }
+
+        IActionResult Error(string message)
+        {
+            _logger.LogError("{error}", message);
+
+            return View(_errorPrivacyPolicy.Value);
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
