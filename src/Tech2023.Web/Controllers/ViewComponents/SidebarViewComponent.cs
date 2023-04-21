be2c702139491700249bf56ed058d672c63b06f0
Zennon-Zen <zennon613@gmail.com>
2023-04-21 23:28:07 +1200

    Make subjects dynamic in Sidebar

--- src/Tech2023.Web/Controllers/ViewComponents/SidebarViewComponent.cs
+++ src/Tech2023.Web/Controllers/ViewComponents/SidebarViewComponent.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Tech2023.Web.Models;
 
 namespace Tech2023.Web.ViewComponents;
 
@@ -8,6 +9,11 @@ public class SidebarViewComponent : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
-        return View();
+        List<Subject> subjects = new List<Subject>();
+        subjects.Add(new Subject("#ef4444", CourseProvider.CAIE, "Maths", ""));
+        subjects.Add(new Subject("#316aff", CourseProvider.CAIE, "Physics", ""));
+        subjects.Add(new Subject("#ff7b16", CourseProvider.CAIE, "Chemistry", ""));
+        subjects.Add(new Subject("#ffe800", CourseProvider.CAIE, "IT", ""));
+        return View(subjects);
     }
 }
