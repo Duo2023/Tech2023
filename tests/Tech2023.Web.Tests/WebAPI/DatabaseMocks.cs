bdc08f3799f265ff98eaecee1ef75091e638f29c
Bradley Grover <ac111897@avcol.school.nz>
2023-04-23 15:36:28 +1200

    add privacy controller unit tests

---
+++ tests/Tech2023.Web.Tests/WebAPI/DatabaseMocks.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tech2023.DAL;
+
+namespace Tech2023.Web.Tests.WebAPI;
+
+internal class TestDbContextFactory : IDbContextFactory<ApplicationDbContext>
+{
+    internal readonly DbContextOptions<ApplicationDbContext> _options;
+
+    public TestDbContextFactory(DbContextOptions<ApplicationDbContext> options)
+    {
+        _options = options;
+    }
+
+    public ApplicationDbContext CreateDbContext()
+    {
+        return new(_options);
+    }
+}
+
+internal class DbOptions
+{
+    internal static readonly DbContextOptions<ApplicationDbContext> _options
+        = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase("TestDb").Options;
+
+
+    public static DbContextOptions<ApplicationDbContext> Get()
+    {
+        return _options;
+    }
+}
