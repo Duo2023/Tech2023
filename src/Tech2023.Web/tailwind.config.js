c2a2afdef4492b0c4756867dfee2274f8083b85a
Zennon-Zen <zennon613@gmail.com>
2023-04-25 21:08:08 +1200

    Add favourite subject section with animations

--- src/Tech2023.Web/tailwind.config.js
+++ src/Tech2023.Web/tailwind.config.js
@@ -3,19 +3,20 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./Views/**/*.cshtml"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Manrope", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: "#3C38FA",
-        secondary: "#191A1F",
-        background: "#131418",
-        hint: "#3E3E42",
-      },
+    content: ["./Views/**/*.cshtml"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#3C38FA",
+                secondary: "#191A1F",
+                background: "#131418",
+                hint: "#3E3E42",
+                "text-hint": "#ACADAE",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
