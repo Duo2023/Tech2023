5ff01bd684b1b3c5df229d4759328777012fa6f9
Bradley Grover <ac111897@avcol.school.nz>
2023-04-26 10:00:58 +1200

    Add basic integration tests

---
+++ tests/Tech2023.Web.IntegrationTests/BasicTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tech2023.Web.IntegrationTests;
+
+public class BasicTests : IClassFixture<WebApplicationFactory<Startup>>
+{
+    private readonly WebApplicationFactory<Startup> _factory;
+
+    public BasicTests(WebApplicationFactory<Startup> factory)
+    {
+        _factory = factory;
+    }
+
+    [Theory]
+    [InlineData(Routes.Home)]
+    [InlineData(Routes.Privacy)]
+    public async Task GetEndpointsReturnSuccess(string url)
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ tests/Tech2023.Web.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tech2023.Web.IntegrationTests;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
+    where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+    }
+}
