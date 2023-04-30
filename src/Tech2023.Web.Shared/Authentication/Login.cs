9d854b6f5da24ce8f9a3881afc5faa690bdc6cea
Zennon-Zen <zennon613@gmail.com>
2023-04-30 19:40:46 +1200

    Make validation error messages more user friendly

--- src/Tech2023.Web.Shared/Authentication/Login.cs
+++ src/Tech2023.Web.Shared/Authentication/Login.cs
@@ -14,15 +14,15 @@ public class Login
 {
     [EmailAddress]
     [Display(Name = nameof(Email), Prompt = "Enter email")]
-    [DataType(DataType.EmailAddress)]
-    [Required(AllowEmptyStrings = false)]
+    [DataType(DataType.EmailAddress, ErrorMessage = "Invalid E-mail address")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter an E-mail address")]
     [JsonPropertyName("email")]
     public string Email { get; set; }
 
     [DataType(DataType.Password)]
     [MinLength(AuthConstants.MinPasswordLength, ErrorMessage = AuthConstants.PasswordLengthError)]
     [Display(Prompt = "Enter password")]
-    [Required(AllowEmptyStrings = false, ErrorMessage = "You must enter a password")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a password")]
     [JsonPropertyName("password")]
     public string Password { get; set; }
 }
