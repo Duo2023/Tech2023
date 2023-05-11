3e228b3ec64ee232293617bd225b90be50259306
Bradley Grover <ac111897@avcol.school.nz>
2023-05-12 09:55:27 +1200

    push typescript updates

--- src/Tech2023.Web/scripts/statistics.ts
+++ src/Tech2023.Web/scripts/statistics.ts
@@ -2,9 +2,7 @@
  * Contains functions and data types for working with the statistics of the web API
  */
 export module statistics {
-    declare let API_BASE_URL: string | undefined;
-
-    const uri: URL = new URL("/statistics/ping", API_BASE_URL);
+    const uri: URL = new URL("api/statistics/ping", API_BASE_URL);
 
     /**
      * The same as Web.Shared/Statistics/PingResponse.cs but in TypeScript
