b9df33db6aa14ec975e07f300990f1aa95526fb8
Bradley Grover <ac111897@avcol.school.nz>
2023-05-23 00:14:36 +1200

    more speeeeeeed

--- src/Tech2023.Core/HtmlHexString.cs
+++ src/Tech2023.Core/HtmlHexString.cs
@@ -1,12 +1,14 @@
-﻿namespace Tech2023.Core;
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace Tech2023.Core;
 
 /// <summary>
 /// Class used to generate CSS Hex Color representation
 /// </summary>
 public static class HtmlHexString
 {
-    /// <summary>One byte in hex is 2 digits, 2 digits * sizeof(uint) = 8. +1 for # character at the start</summary>
-    internal const int BufferSize = 9;
+    internal const int HexBase = 'W';
 
     /// <summary>
     /// Gets the color value as a hexadecimal string value
@@ -14,12 +16,38 @@ public static class HtmlHexString
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe string GetHtmlHexString(uint value)
     {
-        char* chars = stackalloc char[BufferSize];
+        int length = CountLength(value);
+
+        char* chars = stackalloc char[length];
 
         *chars = '#'; // set the first element in memory no matter what to #
 
-        _ = value.TryFormat(new Span<char>(chars + 1, 8), out int written, "x"); // create a span from the 2nd memory element and let the standard library do the formatting
+        int digits = -1;
 
-        return new string(chars, 0, written + 1); // create a new string from the stack buffer only including characters that have been written +1 for #
+        char* p = Int32ToHexChars(chars + length, value, digits);
+
+        Debug.Assert(p == chars + 1);
+
+        return new string(chars, 0, length); // create a new string from the stack buffer only including characters that have been written +1 for #
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static unsafe char* Int32ToHexChars(char* buffer, uint value, int digits)
+    {
+        while (--digits >= 0 || value != 0)
+        {
+            byte digit = (byte)(value & 0xF);
+            *(--buffer) = (char)(digit + (digit < 10 ? (byte)'0' : HexBase));
+            value >>= 4;
+        }
+        return buffer;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static int CountLength(uint value)
+    {
+        // The number of hex digits is log16(value) + 1, or log2(value) / 4 + 1
+        return (BitOperations.Log2(value) >> 2) + 2;
     }
 }
