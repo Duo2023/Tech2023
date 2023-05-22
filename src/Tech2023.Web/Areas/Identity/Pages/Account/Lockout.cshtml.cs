4bf3d0e97011606e972fc407f7449a17895c5e6f
Bradley Grover <ac111897@avcol.school.nz>
2023-05-22 21:16:01 +1200

    do more auth stuff with lockout

---
+++ src/Tech2023.Web/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tech2023.Web.Areas.Identity.Pages.Account;
+
+public class LockoutModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
