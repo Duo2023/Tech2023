da498c9458c68d94efd67de40a2d84dfc4521444
Bradley Grover <ac111897@avcol.school.nz>
2023-05-30 11:02:48 +1200

    add logout support

---
+++ src/Tech2023.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,39 @@
+﻿#nullable disable
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.Diagnostics;
+
+using Tech2023.DAL;
+
+namespace Tech2023.Web.Areas.Identity.Pages.Account;
+
+public class LogoutModel : PageModel
+{
+    internal readonly SignInManager<ApplicationUser> _signInManager;
+
+    public LogoutModel(SignInManager<ApplicationUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    public async Task<IActionResult> OnPost(string returnUrl = null)
+    {
+        await _signInManager.SignOutAsync();
+
+        Debug.WriteLine("user signed out");
+
+        if (returnUrl != null)
+        {
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+            // This needs to be a redirect so that the browser performs a new
+            // request and the identity for the user gets updated.
+            return RedirectToPage();
+        }
+    }
+}
