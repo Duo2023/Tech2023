3f4ca2deb5bddf56ad11cb56d030dbbe7040776e
Bradley Grover <ac111897@avcol.school.nz>
2023-07-17 15:18:40 +1200

    Change EnvironmentConfiguration to be methods that can be inlined

--- src/Tech2023.Core/EnvironmentConfiguration.cs
+++ src/Tech2023.Core/EnvironmentConfiguration.cs
@@ -6,7 +6,7 @@
 public static class EnvironmentConfiguration
 {
     /*
-     * Do not change and mark these fields as const instead of static readonly, this is intentional
+     * Do not change and mark these fields as const instead of static method, this is intentional
      * When used in razor pages the compiler treats a const as if (true) and flag an unreachable error
      * The compiler will optimize away the other branch of if (IsDebug) {} else {} anyway
      */
@@ -14,20 +14,26 @@ public static class EnvironmentConfiguration
     /// <summary>
     /// Whether the executing process was built in Debug
     /// </summary>
-    public static readonly bool IsDebug =
+    public static bool IsDebug
+    {
+        get =>
 #if DEBUG
             true;
 #else
             false;
 #endif
+    }
 
     /// <summary>
     /// Whether the executing process was built in Release
     /// </summary>
-    public static readonly bool IsRelease =
-#if RELEASE
+    public static bool IsRelease
+    {
+        get =>
+#if !DEBUG // Sharplab shows this as xor eax, eax when #if RELEASE true #else false is the condition
             true;
 #else
             false;
 #endif
+    }
 }
