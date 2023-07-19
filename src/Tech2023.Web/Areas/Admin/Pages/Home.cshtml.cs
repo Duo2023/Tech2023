66821a2ea8d835c0849a9b1d72f199aa88395b81
Bradley Grover <ac111897@avcol.school.nz>
2023-07-19 12:30:00 +1200

    Add administrator section

---
+++ src/Tech2023.Web/Areas/Admin/Pages/Home.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tech2023.Web.Areas.Admin.Pages;
+
+public class HomeModel : PageModel
+{
+    public void OnGet()
+    {
+        Debug.WriteLine("Administrator page accessed");
+    }
+}
