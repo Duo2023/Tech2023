b54d3d80cdc39109038c9e75ee3fd0ea2f1d1b94
Bradley Grover <ac111897@avcol.school.nz>
2023-07-30 16:13:19 +1200

    Add coverage for routes and update tests

---
+++ tests/Tech2023.Web.IntegrationTests/Sources/AdminRouteSource.cs
@@ -0,0 +1,21 @@
+﻿using Tech2023.Web.Shared;
+using System.Collections;
+
+namespace Tech2023.Web.IntegrationTests.Sources;
+
+public class AdminRouteSource<TSource> : RouteRetriever, IEnumerable<object[]>
+{
+    public AdminRouteSource() : base(typeof(TSource), Visiblity.Adminstrator)
+    {
+    }
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        foreach (var str in _routes)
+        {
+            yield return new object[] { str };
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
