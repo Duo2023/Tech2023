c4c61362de69634d7e1326112278d503259037e7
Bradley Grover <ac111897@avcol.school.nz>
2023-07-30 17:36:23 +1200

    Fix tests for release db

--- tests/Tech2023.Web.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/Tech2023.Web.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
+using Tech2023.DAL;
 using Tech2023.Web.Shared.Email;
 
 namespace Tech2023.Web.IntegrationTests;
@@ -16,11 +21,17 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
 
         builder.ConfigureServices(services =>
         {
-            var emailOptionsDescriptor = services.FirstOrDefault(descriptor =>
-                descriptor.ServiceType == typeof(IConfigureOptions<EmailOptions>));
+            services.RemoveService<IConfigureOptions<EmailOptions>>();
 
-            if (emailOptionsDescriptor != null)
-                services.Remove(emailOptionsDescriptor);
+            services.RemoveDbContextFactory<ApplicationDbContext>();
+
+            services.AddDbContextFactory<ApplicationDbContext>(options =>
+            {
+                options.EnableDetailedErrors();
+                options.EnableSensitiveDataLogging();
+
+                options.UseInMemoryDatabase($"{Assembly.GetExecutingAssembly().FullName}");
+            });
 
             services.Configure<EmailOptions>(options =>
             {
@@ -32,5 +43,32 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
                 options.Password = "*********";
             });
         });
+    }
+
+}
+
+
+internal static class ServiceExtensions
+{
+    public static IServiceCollection RemoveDbContextFactory<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+    {
+        // TODO: we have to override this error because of the way AddDbContextFactory is designed, look into workarounds
+#pragma warning disable EF1001 // Internal EF Core API usage.
+        return services.RemoveService<IDbContextFactory<TDbContext>>()
+            .RemoveService<TDbContext>()
+            .RemoveService<IDbContextFactorySource<TDbContext>>()
+            .RemoveService<DbContextOptions<TDbContext>>();
+#pragma warning restore EF1001 // Internal EF Core API usage.
+    }
+
+    public static IServiceCollection RemoveService<TService>(this IServiceCollection services)
+    {
+        var service = services.FirstOrDefault(descriptor =>
+            descriptor.ServiceType == typeof(TService));
+
+        if (service != null)
+            services.Remove(service);
+
+        return services;
     }
 }
