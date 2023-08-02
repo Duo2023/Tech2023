1f88d0570bec898428563d26751df39c99422f1e
Bradley Grover <ac111897@avcol.school.nz>
2023-08-02 21:06:55 +1200

    Add comments to HTML hex string

--- src/Tech2023.Core/HtmlHexString.cs
+++ src/Tech2023.Core/HtmlHexString.cs
@@ -8,16 +8,8 @@ namespace Tech2023.Core;
 /// </summary>
 public static class HtmlHexString
 {
-    internal const int HexBase = 'W';
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static uint NextColor()
-    {
-        return (uint)Random.Shared.Next();
-    }
-
     /// <summary>
-    /// Gets the color value as a hexadecimal string value
+    /// Gets the color value as a hexadecimal html string value, eg. #aaffbbcc
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe string GetHtmlHexString(uint value)
@@ -39,15 +31,20 @@ public static class HtmlHexString
         return new string(chars, 0, length); // create a new string from the stack buffer only including characters that have been written +1 for #
     }
 
+    internal const int HexBase = 'W';
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static unsafe char* Int32ToHexChars(char* buffer, uint value, int digits)
     {
-        while (--digits >= 0 || value != 0)
+        while (--digits >= 0 || value != 0) // loop over the hex digits
         {
-            byte digit = (byte)(value & 0xF);
+            byte digit = (byte)(value & 0xF); // get the digit
+
+            // If the digit is in the range 0-9 add it to the ASCII value of 0 and every value will fit into the ASCII numeric digit range between 48 to 57
+            // Else if the digit is 10 >= add it to the base 'W' (87) which we can then add 10-15 onto to fit into the abcdf (97-102) range
             *--buffer = (char)(digit + (digit < 10 ? (byte)'0' : HexBase));
-            value >>= 4;
+
+            value >>= 4; // bitshift for the next number
         }
 
         return buffer;
@@ -57,6 +54,18 @@ public static class HtmlHexString
     internal static int CountLength(uint value)
     {
         // The number of hex digits is log16(value) + 1, or log2(value) / 4 + 1
+        // Add +2 for the # character to be included
         return (BitOperations.Log2(value) >> 2) + 2;
     }
+
+    /// <summary>
+    /// Generate a random unsigned 32-bit integer
+    /// </summary>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint NextColor()
+    {
+        return (uint)Random.Shared.Next(-int.MaxValue, int.MaxValue);
+    }
+
 }
