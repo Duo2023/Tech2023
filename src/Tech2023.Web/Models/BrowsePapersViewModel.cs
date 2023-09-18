a1a397c35997ab7e6a1d30c900e69896629cfca1
Zennon-Zen <zennon613@gmail.com>
2023-09-18 15:19:02 +1200

    Model paper browsing data

---
+++ src/Tech2023.Web/Models/BrowsePapersViewModel.cs
@@ -0,0 +1,11 @@
+using Tech2023.DAL.Models;
+
+namespace Tech2023.Web.Models;
+public class BrowsePapersViewModel
+{
+    public Subject SelectedSubject { get; } = new();
+    public string SelectedCategory { get; } = "Year";
+    public List<string> Categories { get; set; } = new();
+    public string? FilterArgs { get; set; }
+    public Resource? SelectedPaper {get; set; }
+}
--- src/Tech2023.Web/Models/Components/SidebarInputModel.cs
+++
@@ -1,8 +0,0 @@
-using Tech2023.DAL.Models;
-
-namespace Tech2023.Web.Models;
-public class SidebarInputModel
-{
-    public string? FilterArgs { get; set; }
-    public Subject? SelectedSubject { get; set; }
-}
