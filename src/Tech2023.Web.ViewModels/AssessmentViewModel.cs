08e01d4ea99e15818ec093144434abbed2523df8
Bradley Grover <ac111897@avcol.school.nz>
2023-10-01 18:37:03 +1300

    Unify several view model implementations

---
+++ src/Tech2023.Web.ViewModels/AssessmentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Tech2023.DAL;
+
+namespace Tech2023.Web.ViewModels;
+
+#nullable disable
+
+/// <summary>
+/// Used for displaying an NCEA assessment
+/// </summary>
+public class NceaAssessmentViewModel : AssessmentViewModel<NceaResource> { };
+
+/// <summary>
+/// Used for displaying a Cambridge assessment
+/// </summary>
+public class CambridgeAssessmentViewModel : AssessmentViewModel<CambridgeResource> { };
+
+/// <summary>
+/// Serves as a base class for an assessment view model implementation
+/// </summary>
+/// <typeparam name="TResource">The assessment resource to use</typeparam>
+public abstract class AssessmentViewModel<TResource> where TResource : CustomResource
+{
+    public SubjectViewModel Subject { get; set; }
+
+    public TResource Resource { get; set; }
+}
--- src/Tech2023.Web.ViewModels/ItemViewModel.cs
+++ src/Tech2023.Web.ViewModels/ItemViewModel.cs
@@ -5,15 +5,34 @@ namespace Tech2023.Web.ViewModels;
 
 #nullable disable
 
-public class ItemViewModel<TResource> where TResource : CustomResource
-{
-    public SubjectViewModel Subject { get; set; }
-
-    public TResource Resource { get; set; }
-
-    public Item Item { get; set; }
-}
-
+/// <summary>
+/// Used for displaying an NCEA assessment item
+/// </summary>
 public class NceaItemViewModel : ItemViewModel<NceaResource> { }
 
+/// <summary>
+/// Used for displaying a Cambridge assessment item
+/// </summary>
 public class CambridgeItemViewModel : ItemViewModel<CambridgeResource> { }
+
+/// <summary>
+/// Used for displaying an assessment item
+/// </summary>
+/// <typeparam name="TResource">The assessment resource to use</typeparam>
+public abstract class ItemViewModel<TResource> where TResource : CustomResource
+{
+    /// <summary>
+    /// The subject that the <see cref="Item"/> belongs to
+    /// </summary>
+    public SubjectViewModel Subject { get; set; }
+
+    /// <summary>
+    /// The resource that the <see cref="Item"/> belongs to
+    /// </summary>
+    public TResource Resource { get; set; }
+
+    /// <summary>
+    /// The item to display
+    /// </summary>
+    public Item Item { get; set; }
+}
