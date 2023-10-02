dfab1ce0273406cd152bb148d0807560f62a495f
Bradley Grover <ac111897@avcol.school.nz>
2023-10-02 23:08:22 +1300

    Make script parameter based

--- src/add_migration.ps1
+++ src/add_migration.ps1
@@ -1,9 +1,12 @@
-$migrationName=$args[0];
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$name
+)
 
-if ([string]::IsNullOrWhiteSpace($migrationName)) {
+if ([string]::IsNullOrWhiteSpace($name)) {
     Write-Error "Cannot create a migration becuase the migration name is null or empty"
 } 
 else
 {
-    dotnet-ef migrations add $migrationName --context ApplicationDbContext --startup-project Tech2023.Web --project Tech2023.DAL.Migrations --configuration release
+    dotnet-ef migrations add $name --context ApplicationDbContext --startup-project Tech2023.Web --project Tech2023.DAL.Migrations --configuration release
 }
