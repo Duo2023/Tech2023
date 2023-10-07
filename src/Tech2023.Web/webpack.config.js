01b7ff1d352784577ae71b3a9a771f7680a9dd5b
Zennon <zennon613@gmail.com>
2023-10-08 11:53:53 +1300

    Code formatting and annotations

--- src/Tech2023.Web/webpack.config.js
+++ src/Tech2023.Web/webpack.config.js
@@ -1,16 +1,22 @@
 const { merge } = require("webpack-merge");
+
+// Base config:
 const commonConfig = require("./webpack/webpack.common.js");
+// Custom configs respective to the libraries:
 const libconfigs = [
     require("./scripts/interactions/webpack.config.js") /*pdf-js-dist: , require("./scripts/pdf/webpack.config.js")*/,
 ];
+// Prod and Dev specific configs to apply to previous configs:
 const prodConfig = require("./webpack/webpack.prod.js");
 const devConfig = require("./webpack/webpack.dev.js");
 
 function setupConfigs(modeConfig) {
     let configs = libconfigs.map((lib) => {
+        // Apply order: base <- lib <- mode (prod or dev):
         return merge(commonConfig, lib, modeConfig);
     });
 
+    // Clean the output folder before compiling:
     configs[0] = merge(configs[0], { output: { clean: true } });
 
     return configs;
